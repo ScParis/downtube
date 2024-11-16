40ade7c6f4b807b363c0681b375721d447d39a83
ScParis <schpariss@gmail.com>
2024-11-15 21:47:55 -0300

    start django with first page

---
+++ dt_main/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class DtMainConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'dt_main'
---
+++ dt_main/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+import datetime
+
+def home(request):
+    now = datetime.datetime.now()
+    html = f'<html><body><H1>It is now {now}</H1></body></html>'
+    return HttpResponse(html)
